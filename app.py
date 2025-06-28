620e2a9e077c36f8cb34c3c86642455ec043cfdb
vini0517 <vinitha0517@gmail.com>
2025-06-28 12:47:07 +0530

    Ready for Render deployment

--- app.py
+++ app.py
@@ -242,4 +242,5 @@ def upload():
         return jsonify({"error": "An error occurred! Please check the logs for details."}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render dynamically assigns port
+    app.run(host='0.0.0.0', port=port)
